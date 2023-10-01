777c112d71b01a8306b997d9031387fa07171695
Saikalyan Labhishetty <89219380+SaikalyanLabhishetty@users.noreply.github.com>
2023-10-01 16:20:49 +0530

    Update nodemailer.js

--- nodemailer.js
+++ nodemailer.js
@@ -6,6 +6,12 @@ const app = express();
 const port = 3000;
 
 
+app.use(express.static((__dirname, "public")));
+app.use('/css',express.static(__dirname+'/public/css'))
+app.use('/js',express.static(__dirname+'/public/js'))
+app.use('/html',express.static(__dirname+'/public/html'))
+app.use(express.static('image'));
+
 // Define a route to serve your HTML files
 app.get('/index.html', (req, res) => {
   res.sendFile(__dirname + '/index.html'); 
@@ -18,10 +24,14 @@ app.get('/project.html', (req, res) => {
 app.get('/contact.html', (req, res) => {
     res.sendFile(__dirname + '/contact.html'); 
 });
+app.get('/success.html', (req, res) => {
+  res.sendFile(__dirname + '/public/html/success.html'); 
+});
 
-app.use(express.static((__dirname, "public")));
+app.get('/error.html', (req, res) => {
+  res.sendFile(__dirname + '/public/html/error.html'); 
+});
 
-app.use(express.static('image'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -33,9 +43,9 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
+// app.get('/', (req, res) => {
+//   res.sendFile(__dirname + '/index.html');
+// });
 
 app.post('/sendEmail', (req, res) => {
   const { name, email, phone,design_type,date } = req.body;
@@ -55,7 +65,7 @@ app.post('/sendEmail', (req, res) => {
       res.send('Error: Email could not be sent.');
     } else {
       console.log('Email sent:', info.response);
-      res.send('Email sent successfully.');
+      res.redirect('/success.html');
     }
   });
 });
